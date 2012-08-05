227aac02e7b8c1ad05cc9d11ebb8f38850b798bf
Sven Ehrke <sven.ehrke@canoo.com>
2012-08-05 20:50:21 +0200

    extended tests

--- src/test/java/org/svenehrke/buildergenerator/namedconstruction/NCBuilderGeneratorTest.java
+++ src/test/java/org/svenehrke/buildergenerator/namedconstruction/NCBuilderGeneratorTest.java
@@ -32,5 +32,25 @@ public class NCBuilderGeneratorTest {
 			),
 			new NCBuilderGenerator("Foo", "R,String,required1").generate());
 	}
+	@Test
+	public void generate_Foo_with_2required_3optional() throws Exception {
+		assertEquals(
+			toText(
+				outClassLine("Foo"),
+				outAttributeLine("String", "required1"),
+				outAttributeLine("int", "required2"),
+				outAttributeLine("BigDecimal", "optional1"),
+				outAttributeLine("BigInteger", "optional2"),
+				outAttributeLine("double", "optional3"),
+				outEndLine()
+			),
+			new NCBuilderGenerator("Foo"
+				, "R,String,required1"
+				, "R,int,required2"
+				, "O,BigDecimal,optional1"
+				, "O,BigInteger,optional2"
+				, "O,double,optional3"
+			).generate());
+	}
 
 }
